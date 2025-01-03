6a2b3f4ee3edfa732ab3a79a462b95da81dbe24b
atrevinyo <you@exemple.com>
2025-01-03 14:33:03 +0100

    Compilació del frondend mode producció 3

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 
 
 export const environments = {
-  baseURL: "http://localhost:3000"
-  // baseURL: "tfm-dw-backend-production.up.railway.app"
+  // baseURL: "http://localhost:3000"
+  baseURL: "tfm-dw-backend-production.up.railway.app"
 }
